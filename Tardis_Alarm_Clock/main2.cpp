7d5ba6e96130f27954c535a6502acf86513736b8
Scott Goldthwaite <scott@goldthwaite.com>
2014-01-26 19:13:56 -0500

    EmbedXcode+ Upgrade
    Changes as part of upgrading to EmbedXcode+

--- Tardis_Alarm_Clock/main2.cpp
+++ Tardis_Alarm_Clock/main2.cpp
@@ -2,16 +2,16 @@
 // main.cpp
 // Main file
 // ----------------------------------
-// Developed with embedXcode
+// Developed with embedXcode+
 // http://embedXcode.weebly.com
 //
-// Project 		Tardis_Alarm_Clock
+// Project 		Tardis_Alarm_Clock Library
 //
-// Created by 	Scott, 10/22/13 9:17 AM
-//              Scott Goldthwaite
+// Created by 	Scott Goldthwaite, 1/26/14 7:04 PM
+// 				Scott Goldthwaite
 //
-// Copyright 	© Scott, 2013
-// License 		<#license#>
+// Copyright 	© Scott Goldthwaite, 2014
+// License 		GNU General Public License
 //
 // See 			Tardis_Alarm_Clock.ino and ReadMe.txt for references
 //
@@ -32,52 +32,64 @@
 #if defined(MPIDE)
 // ============================================================================= chipKIT specific
 
-// **********************************************************************
-// 	main.c
-//
-// 	Arduino core files for PIC32
-// 		Copyright (c) 2010, 2011 by Mark Sproul
-//
-//
-// **********************************************************************
-// 	this code is based on code Copyright (c) 2005-2006 David A. Mellis
-//
-// 	This library is free software; you can redistribute it and/or
-// 	modify it under the terms of the GNU Lesser General Public
-// 	License as published by the Free Software Foundation; either
-// 	version 2.1 of the License, or (at your option) any later version.
-//
-// 	This library is distributed in the hope that it will be useful,
-// 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-// 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.// 	See the GNU
-// 	Lesser General Public License for more details.
-//
-// 	You should have received a copy of the GNU Lesser General
-// 	Public License along with this library; if not, write to the
-// 	Free Software Foundation, Inc., 59 Temple Place, Suite 330,
-// 	Boston, MA  02111-1307  USA
-//
-//
-// **********************************************************************
-// 	Edit History
-// **********************************************************************
-// 	Oct 12,	2010	Got MPLAB X working on MacOSX 1.6 for the first time
-//  Dec 12, 2011 <GeneApperson> added call to _scheduleTask() before call
-// 					to loop().
-// **********************************************************************
+//************************************************************************
+//*	main.c
+//*
+//*	Arduino core files for PIC32
+//*		Copyright (c) 2010, 2011 by Mark Sproul
+//*
+//*
+//************************************************************************
+//*	this code is based on code Copyright (c) 2005-2006 David A. Mellis
+//*
+//*	This library is free software; you can redistribute it and/or
+//*	modify it under the terms of the GNU Lesser General Public
+//*	License as published by the Free Software Foundation; either
+//*	version 2.1 of the License, or (at your option) any later version.
+//*
+//*	This library is distributed in the hope that it will be useful,
+//*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.//*	See the GNU
+//*	Lesser General Public License for more details.
+//*
+//*	You should have received a copy of the GNU Lesser General
+//*	Public License along with this library; if not, write to the
+//*	Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+//*	Boston, MA  02111-1307  USA
+//*
+//*
+//************************************************************************
+//*	Edit History
+//************************************************************************
+//*	Oct 12,	2010	Got MPLAB X working on MacOSX 1.6 for the first time
+//* Dec 12, 2011 <GeneApperson> added call to _scheduleTask() before call
+//*					to loop().
+//************************************************************************
 
 #define OPT_SYSTEM_INTERNAL
-#include "System_Defs.h"
-#include "WProgram.h"
+#include <System_Defs.h>
 
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+extern "C" {
+    extern void __use_isr_install(void);
+    __attribute__((section(".comment"))) void (*__use_force_isr_install)(void) = &__use_isr_install;
+}
+
+//************************************************************************
 int main(void)
 {
 	init();
     
 	setup();
     
-	for (;;) {
-        //		_scheduleTask();
+	while (1)
+	{
+		_scheduleTask();
 		loop();
 	}
 	return 0;
@@ -103,10 +115,34 @@ int main(void)
 
 
 
+#elif defined(MICRODUINO)
+// ============================================================================= Microduino specific
+
+#include "Arduino.h"
+
+int main(void)
+{
+	init();
+    
+#if defined(USBCON)
+	USBDevice.attach();
+#endif
+	
+	setup();
+    
+	for (;;) {
+		loop();
+		if (serialEventRun) serialEventRun();
+	}
+    
+	return 0;
+}
+
+
 #elif defined(ENERGIA)
 // ============================================================================= LaunchPad MSP430, Stellaris and Tiva, Experimeter Board FR5739 specific
 
-#if defined(__LM4F120H5QR__) or defined(__TM4C1230C3PM__)
+#if defined(__LM4F120H5QR__)
 // ----------------------------------------------------------------------------- LaunchPad Stellaris and Tiva specific
 
 #include "Energia.h"
@@ -150,7 +186,7 @@ int main(void)
 }
 
 #else
-// ----------------------------------------------------------------------------- LaunchPad MSP430 and Experimeter Board FR4739 specific
+// ----------------------------------------------------------------------------- LaunchPad MSP430 and Experimeter Board FR5739 specific
 
 #include "Energia.h"
 
